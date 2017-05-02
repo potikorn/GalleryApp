305047494aa5cb566070f7fbd4746d15d177745d
Potikorn <potikornth@gmail.com>
2017-05-02 21:30:06 +0700

    first commit

---
+++ app/src/main/java/com/example/android/galleryapp/helper/SquareLayout.java
@@ -0,0 +1,36 @@
+package com.example.android.galleryapp.helper;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by potik on 02-May-17.
+ */
+
+public class SquareLayout extends RelativeLayout{
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
